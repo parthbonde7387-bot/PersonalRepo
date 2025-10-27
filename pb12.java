4d5ed6c6aee377bec96290b72d54bcce8061a0ab
Parth <your@email.com>
2025-10-27 21:03:19 +0530

    first commit

---
+++ pb12.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // circumference = 2 * Math.PI * radius;
+        // area = Math.PI * Math.pow(radius, 2);
+        // valume  = (4.0 / 3.0) * Math.pow(radius, 3);
+
+        Scanner scanner = new Scanner(System.in);
+
+        double radius;
+        double circumference;
+        double area;
+        double volume;
+
+        System.out.println("Enter the radius: ");
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+
+        System.out.printf("The circumference is : %.1fcm\n", circumference);
+        System.out.printf("The area is: %.1fcm^2 \n", area );
+        System.out.printf("The volume is: %.1fcm^3\n", volume);
+
+        scanner.close();
+    }
+}
